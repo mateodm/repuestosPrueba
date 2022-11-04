63ade023b0341996648ceb78a67c2bb19f958a62
Mateo DM <104037425+mateodm@users.noreply.github.com>
2022-11-03 23:46:52 -0300

    intentos

--- main.js
+++ main.js
@@ -11,24 +11,29 @@ document.addEventListener("keyup", e => {
         })
     }
 })
- /* FILTRO POR CATEGORIAS */
- /*
-document.addEventListener("click" e => {
-    if (e.target.matches("#categoria")) {
-        document.querySelectorAll(".botonCategoria").forEach(categoriaFiltrado => {
-            categoriaFiltrado.textContent.includes(e.target.value)
-            ?categoriaFiltrado.classList.remove("filtro")
-            :categoriaFiltrado.classList.add("filtro")
 
-        })
-    }
-})
- */
 
 /* SORT MAYOR A MENOR - MENOR A MAYOR (POR AHORA SOLO FUNCIONA EN CONSOLA)*/
+let validador = "invalido"
 function menorSort() {
     stock.sort((a,b) => a.precio - b.precio);
-    return producto
+    console.log(stock)
+    stock.forEach((producto) => {
+        const div = document.createElement("div")
+        div.classList.add("producto")
+        div.innerHTML = `
+            <div class="card col-md-12 mb-4">
+                <img id="imagen" src=${producto.img} class="card-imagen card-img-top my-3" alt="Panel Led Indoor 100w"/>
+            <div class="card-body">
+                <h5 id="nombre" class="card-title"> ${producto.nombre}</h5>
+                <h5 style="display: none;"> ${producto.categoria} </h5>
+                <p id="precio" class="card-text card-precio"> $${producto.precio} ars</p>
+                <button onclick="agregarAlCarrito(${producto.id})" class="btn d-block btn-primary botones_productos">Agregar al carrito</button>
+            </div>
+            </div>
+    `
+    ubicacionProductos.appendChild(div)
+    })
 }
 function mayorSort() {
     stock.sort((a,b) => b.precio - a.precio);
--- stock.js
+++ stock.js
@@ -15,25 +15,28 @@ let stock = [
     {id: 3, nombre: "Bateria", categoria: "Electronica", cantidad: 1, precio: 12000, img:"./imagenes/bateria.png"},
     {id: 4, nombre: "Espejo retrovisor", categoria: "Carroceria", cantidad: 1, precio: 16000, img:"./imagenes/espejoretrovisor.png"},
     {id: 5, nombre: "Moldura", categoria: "Carroceria", cantidad: 1, precio: 18000, img:"./imagenes/moldura.png"},
-    {id: 6, nombre: "Etc", categoria: "undefined", cantidad: 1, precio: 7000, img:"./imagenes/logotemp-pagina.gif"},
+    {id: 6, nombre: "Rueda", categoria: "undefined", cantidad: 1, precio: 15900, img:"./imagenes/logotemp-pagina.gif"},
     {id: 7, nombre: "Unproducto", categoria: "undefined", cantidad: 1, precio: 2000, img:"./imagenes/logotemp-pagina.gif"},
     {id: 8, nombre: "Repuesto8", categoria: "undefined", cantidad: 1, precio: 1500, img:"./imagenes/logotemp-pagina.gif"},
+    
 ]
-
 const ubicacionProductos = document.getElementById("cardsProductos")
-stock.forEach((producto) => {
-const div = document.createElement("div")
-div.classList.add("producto")
-div.innerHTML = `
-<div class="card col-md-12 mb-4">
-<img id="imagen" src=${producto.img} class="card-imagen card-img-top my-3" alt="Panel Led Indoor 100w"/>
-<div class="card-body">
-<h5 id="nombre" class="card-title"> ${producto.nombre}</h5>
-<h5 style="display: none;"> ${producto.categoria} </h5>
-<p id="precio" class="card-text card-precio"> $${producto.precio} ars</p>
-<button onclick="agregarAlCarrito(${producto.id})" class="btn d-block btn-primary botones_productos">Agregar al carrito</button>
-</div>
-</div>
+    stock.forEach((producto) => {
+    const div = document.createElement("div")
+    div.classList.add("producto")
+    div.innerHTML = `
+        <div class="card col-md-12 mb-4">
+            <img id="imagen" src=${producto.img} class="card-imagen card-img-top my-3" alt="Panel Led Indoor 100w"/>
+        <div class="card-body">
+            <h5 id="nombre" class="card-title"> ${producto.nombre}</h5>
+            <h5 style="display: none;"> ${producto.categoria} </h5>
+            <p id="precio" class="card-text card-precio"> $${producto.precio} ars</p>
+            <button onclick="agregarAlCarrito(${producto.id})" class="btn d-block btn-primary botones_productos">Agregar al carrito</button>
+        </div>
+        </div>
 `
 ubicacionProductos.appendChild(div)
 })
+
+
+
